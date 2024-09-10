31c55073d760ba89eb807b4fd05c2ae67602108d
Eugene Pushkin <eugene.pushckin@yandex.ru>
2024-09-10 11:22:49 +0300

    feat/add history(updated)

--- js/clear.js
+++ js/clear.js
@@ -1,11 +1,7 @@
-import { defaultValueLocalStorage } from './constants/local.js';
-import { localStorageCount } from './main.js'
-
 const clearHistoryBtn = document.querySelector('.clear-btn');
 
 function clearHistory() {
     localStorage.clear();
-    localStorageCount = defaultValueLocalStorage;
 };
 
 clearHistoryBtn.addEventListener("click", clearHistory);
