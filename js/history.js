78ab73fd7e87e65ef94a084218cfbb76fdd021d8
Eugene Pushkin <eugene.pushckin@yandex.ru>
2024-09-10 10:49:18 +0300

    feat/add history(updated)

--- js/history.js
+++ js/history.js
@@ -8,7 +8,6 @@ export function historyUpdate() {
     } else {
         historyOperations.innerHTML = ``;
         for (let i = 0; i < localStorage.length; i++) {
-            console.log(localStorage[i][0] === '-')
             if (regCollectionOfNumbers.test(localStorage[i][0])) {
                 historyOperations.insertAdjacentHTML('beforeend', `<p>${localStorage[i]}</p>`)
             } else if (localStorage[i][0] === '-') {
