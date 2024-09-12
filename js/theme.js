13aadea300d76814faa41c56350a0c53e7607935
Eugene Pushkin <eugene.pushckin@yandex.ru>
2024-09-12 13:53:20 +0300

    feat/add theme(updated)

--- js/theme.js
+++ js/theme.js
@@ -4,15 +4,14 @@ const selectForm = document.querySelector('.theme-select');
 const body = document.querySelector('body');
 const selectDarkTheme = selectForm[1];
 
-if (localStorage.theme === 'dark-theme') {
-    selectDarkTheme.selected = true;
-};
-
 function themeChanger() {
     localStorageMethods.delete('theme');
     localStorageMethods.setTheme('theme', `${selectForm.value}`);
     if (localStorage.theme === 'dark-theme') body.classList.add('dark');
     else body.classList.remove('dark');
+    if (localStorage.theme === 'dark-theme') {
+        selectDarkTheme.selected = true;
+    };
 };
 
 selectForm.addEventListener("click", themeChanger);
